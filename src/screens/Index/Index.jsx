e3585441b0049a64eb5d67a4a43cbe0ecf75f0c9
jlabudzik <36515019+jlabudzik@users.noreply.github.com>
2023-08-17 09:01:52 -0400

    Initial commit

---
+++ src/screens/Index/Index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <div className="text-wrapper">Jack’s Website</div>
+        <div className="overlap-group">
+          <div className="text-wrapper-2">click me</div>
+        </div>
+      </div>
+    </div>
+  );
+};
